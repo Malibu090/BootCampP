740d26038a59bf7eaa7bf75fbf0b5adff196b75b
Malibu <malibu090@mail.ru>
2023-01-11 18:04:33 +0300

    Первая лекция

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+// 1 5 4 1 3 4 7 4 1 2
+// m = 3
+// MAX -> 15
+
+using System.Diagnostics;
+
+int size = 1000000;
+int m = 100000;
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max) max = t;
+
+}
+
+sw.Stop();
+System.Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
